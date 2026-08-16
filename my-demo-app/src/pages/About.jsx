090d0ccdac334cf8ad3f91a1b96cbb8f4586df63
motheeswaran2805@gmail.com <motheeswaran2805@gmail.com>
2026-08-16 20:33:03 +0530

    Homepages

---
+++ my-demo-app/src/pages/About.jsx
@@ -0,0 +1,10 @@
+export default function About(){
+    
+
+    return(
+        <>
+        <h1>Introduction </h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit a ipsa velit ipsam sint, dolore omnis ab quis sunt impedit quasi, fugit voluptatem earum eaque eveniet exercitationem quo, voluptate reiciendis?</p>
+        </>
+    );
+}
---
+++ my-demo-app/src/pages/Career.jsx
@@ -0,0 +1,9 @@
+export default function Career(){
+
+    return(
+        <>
+        <h1>Courses</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi quas recusandae aliquam unde adipisci eaque sint laborum! Nam odit ex qui in ipsam, soluta sequi quae officiis aut repellat culpa.</p>
+        </>
+    )
+}
